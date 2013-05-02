f43239bcdf0c644eedcb76e4152a0b73cd1e77f0
Alex Brown <alex@alexanderdbrown.com>
2013-05-02 18:55:59 +0100

    Front page.

--- kyffin/__init__.py
+++ kyffin/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 def run():
     app=Flask("Kyffin")
     
     @app.route("/")
     def hello():
-        return "Hello World"
+        return render_template("home.html")
 
-    app.run()
+    app.run(debug=True)
